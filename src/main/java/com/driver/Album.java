3b7332e4b4f5b1bf838a6c5425e9cd766af6b4f1
yeduruswapna <swapnayeduru@gmail.com>
2023-06-01 16:21:33 +0530

    First Commit

--- src/main/java/com/driver/Album.java
+++ src/main/java/com/driver/Album.java
@@ -1,19 +1,23 @@
 package com.driver;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Album {
     private String title;
     private Date releaseDate;
+    private List<Song>songs;
 
     public Album(){
+        this.songs=new ArrayList<>();
 
     }
 
     public Album(String title){
         this.title = title;
         this.releaseDate = new Date();
+        this.songs=new ArrayList<>();
     }
 
     public String getTitle() {
@@ -30,5 +34,20 @@ public class Album {
 
     public void setReleaseDate(Date releaseDate) {
         this.releaseDate = releaseDate;
+    }
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public void removeSong(Song song) {
+        songs.remove(song);
     }
 }
--- src/main/java/com/driver/Playlist.java
+++ src/main/java/com/driver/Playlist.java
@@ -1,11 +1,20 @@
 package com.driver;
 
+import com.google.common.util.concurrent.Service;
+
+import java.net.http.WebSocket;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Playlist {
     private String title;
+    private List<Song> songs;
+    private User creator;
+    private List <User>listeners;
 
     public Playlist(){
+        this.songs= new ArrayList<>();
+        this.listeners= new ArrayList<>();
 
     }
 
@@ -20,4 +29,31 @@ public class Playlist {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public void removeSong(Song song) {
+        songs.remove(song);
+    }
+    public User getCreator() {
+        return creator;
+    }
+    public boolean hasListener(User user) {
+        return listeners.contains(user);
+    }
+    public void addUser(User user) {
+        listeners.add(user);
+    }
+
+
 }
